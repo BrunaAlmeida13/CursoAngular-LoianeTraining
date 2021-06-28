75c41d8d1335cb7d80e3a53e722dcf8a45aef3b1
BrunaAlmeida13 <bruna.a.almeida@hotmail.com>
2021-06-28 11:00:53 -0300

    aulas01-08

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -1,13 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { CursosService } from './cursos.service';
 
 @Component({
   selector: 'app-cursos',
-  templateUrl: './cursos.component.html',
+  templateUrl: './cursos.component.html', //template - tag html inline (max 3 linhas), templateUrl - referenciando um arquivo .html
   styleUrls: ['./cursos.component.css']
 })
 export class CursosComponent implements OnInit {
+  //Ex.: Se a URL do portal vem de outro lugar, tem que se criar uma variável na classe do component
+  nomePortal: string
+  cursos: string[]
 
-  constructor() { }
+  //Colocando nos parâmetros do constructor, n há a necessidade de instanciar e criar uma variável local.
+
+  constructor(private cursosService: CursosService) {
+    this.nomePortal = 'http://loiane.training'
+    
+    this.cursos = this.cursosService.getCursos()
+  }
 
   ngOnInit(): void {
   }
--- src/app/cursos/cursos.module.ts
+++ src/app/cursos/cursos.module.ts
@@ -1,15 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CursosComponent } from './cursos.component';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
+import { CursosService } from './cursos.service';
 
 
 
 @NgModule({
   declarations: [
-    CursosComponent
+    CursosComponent,
+    CursoDetalheComponent
   ],
   imports: [
     CommonModule
+  ],
+  exports: [ //dizendo qual component/diretiva/pippe se quer expor para outros módulos
+    CursosComponent
+  ],
+  providers: [
+    CursosService
   ]
 })
 export class CursosModule { }

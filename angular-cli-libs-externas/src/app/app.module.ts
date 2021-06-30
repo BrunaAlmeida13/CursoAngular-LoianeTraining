9076dd9564028e2bf1a44fd7794944188f67c012
BrunaAlmeida13 <bruna.a.almeida@hotmail.com>
2021-06-30 16:43:18 -0300

    modulo-angularCLI-concluido

---
+++ angular-cli-libs-externas/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DiretivaNgIfComponent } from './diretiva-ng-if/diretiva-ng-if.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DiretivaNgIfComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

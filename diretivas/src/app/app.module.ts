d22f50ccfd79cd4aa80f541dde3ea5d7d49ada32
BrunaAlmeida13 <bruna.a.almeida@hotmail.com>
2021-07-01 08:10:30 -0300

    modulo-diretiva-operadorElvis

---
+++ diretivas/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
+import { DiretivaNgforComponent } from './diretiva-ngfor/diretiva-ngfor.component';
+import { DiretivaNgswitchComponent } from './diretiva-ngswitch/diretiva-ngswitch.component';
+import { DiretivaNgclassComponent } from './diretiva-ngclass/diretiva-ngclass.component';
+import { DiretivaNgstyleComponent } from './diretiva-ngstyle/diretiva-ngstyle.component';
+import { OperadorElvisComponent } from './operador-elvis/operador-elvis.component';
+import { NgContentComponent } from './ng-content/ng-content.component';
+import { ExemploNgContentComponent } from './exemplo-ng-content/exemplo-ng-content.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DiretivaNgifComponent,
+    DiretivaNgforComponent,
+    DiretivaNgswitchComponent,
+    DiretivaNgclassComponent,
+    DiretivaNgstyleComponent,
+    OperadorElvisComponent,
+    NgContentComponent,
+    ExemploNgContentComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
--- diretivas/src/app/operador-elvis/operador-elvis.component.ts
+++ diretivas/src/app/operador-elvis/operador-elvis.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OperadorElvisComponent implements OnInit {
 
+  tarefa: any = {
+    desc: 'Descrição da tarefa',
+    resposavel: null
+  }
+
   constructor() { }
 
   ngOnInit(): void {

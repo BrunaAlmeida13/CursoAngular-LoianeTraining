1f5dc52c5bc4cb0599806bf6b845d13f3d37c3e1
BrunaAlmeida13 <bruna.a.almeida@hotmail.com>
2021-07-06 09:05:34 -0300

    modulo-rotas-incompleto

---
+++ rotas/src/app/app-routing.module.ts
@@ -0,0 +1,33 @@
+import { NgModule, Component, ModuleWithProviders } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { CursosComponent } from './cursos/cursos.component';
+
+const APP_ROUTES: Routes = [
+  { path: 'cursos', component: CursosComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '', component: HomeComponent }
+];
+
+//Essa variável é a que contém a definição e a configuração das rotas no projeto
+//.forRoot contém todas as rotas raíz/chave/principal da aplicação
+/*
+RouterModule - Representa um módulo de rotas
+Pra se declarar rotas de funcionalidade, exemplo, se tiver o cursos e quiser tratar isso como funcionalidade, e no cursos pode ter um cursos/id, cursos/editar, pra não poluir esse arquivo app-routing, da mesma forma que se faz com módulos, pode fazer com rotas
+E a única diferença entre rota raíz e de funcionalidade é a chamada do forRoot ou do forChild
+E como é necessário configurar a rota no módulo, põe-se o exportar
+*/
+export const routing: ModuleWithProviders<any> = RouterModule.forRoot(APP_ROUTES)
+
+@NgModule({
+  imports: [RouterModule.forRoot(APP_ROUTES)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+
+/*
+path - Qual o caminho para determinado component
+HomeComponent - Quando se acessar o caminho sem uma hash, faz-se com que o Angular 2 renderize um component
+*/
---
+++ rotas/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {HttpModule} from '@angular/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { CursosComponent } from './cursos/cursos.component';
+import { routing } from './app-routing.module';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    CursosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpModule,
+    routing
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
